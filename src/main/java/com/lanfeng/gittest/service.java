40d2b32ce78917d5912a7d0e012b61638ccbfad8
Ludanr <jxlanfeng22@163.com>
2020-06-12 15:17:47 +0800

    分支第二次更新

--- src/main/java/com/lanfeng/gittest/service.java
+++ src/main/java/com/lanfeng/gittest/service.java
@@ -11,5 +11,6 @@ public class service {
         System.out.println("第二次更新");
         System.out.println("第三次更新");
         System.out.println("第一次分支更新");
+        System.out.println("分支第二次更新");
     }
 }

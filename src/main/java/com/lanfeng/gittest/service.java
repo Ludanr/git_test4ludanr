97e9bcc4f32b8d54df30c4baf55599e8077d4f86
Ludanr <jxlanfeng22@163.com>
2020-06-12 15:12:28 +0800

    第一次提交

---
+++ src/main/java/com/lanfeng/gittest/service.java
@@ -0,0 +1,13 @@
+package com.lanfeng.gittest;
+
+/**
+ * @author lufeng
+ * @create 2020-06-12 15:06
+ */
+public class service {
+    private String name;
+    public static void main(String[] args) {
+        System.out.println("第一次更新");
+        System.out.println("第二次更新");
+    }
+}
